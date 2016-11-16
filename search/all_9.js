48481adc42c50ffa86b86b6fd2e694072327df4c
Xun Wang <wang.xun@gmail.com>
2016-11-16 12:56:01 +1100

    doc update

--- search/all_9.js
+++ search/all_9.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['pauseaudioid',['pauseAudioID',['../classPyNAO.html#ab1ad8e58fbeb41c54e9c5ff7efbc858e',1,'PyNAO']]],
   ['playaudioid',['playAudioID',['../classPyNAO.html#afe6e1c7467359b8f6dc2106f720b48c0',1,'PyNAO']]],
-  ['plusechestled',['pluseChestLED',['../classPyNAO.html#a6d67758b31f62515920f608152c8dcd4',1,'PyNAO']]],
+  ['pulsechestled',['pulseChestLED',['../classPyNAO.html#ab58a7cab94f9cdc8898c4c3482462a8e',1,'PyNAO']]],
   ['pynao',['PyNAO',['../classPyNAO.html',1,'']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -4,6 +4,7 @@ var searchData=
   ['movearmwithjointtrajectory',['moveArmWithJointTrajectory',['../classPyNAO.html#a75d4f3e176f2364501bd51337706a07a',1,'PyNAO']]],
   ['movebodyto',['moveBodyTo',['../classPyNAO.html#af2ebc3aa18760137a198dc6b9ec999d1',1,'PyNAO']]],
   ['movebodywithjointpos',['moveBodyWithJointPos',['../classPyNAO.html#a4bb71603b06d25552d29e67f6e7d83f9',1,'PyNAO']]],
-  ['moveheadto',['moveHeadTo',['../classPyNAO.html#ae0e1ca758698cef8f8f8d24e325e4c40',1,'PyNAO']]],
+  ['movebodywithrawtrajectorydata',['moveBodyWithRawTrajectoryData',['../classPyNAO.html#a24725a18da66b5808b0bb4612794e37d',1,'PyNAO']]],
+  ['moveheadto',['moveHeadTo',['../classPyNAO.html#afd0afd54a46ae509c0ae03e8fbb85166',1,'PyNAO']]],
   ['movelegwithjointpos',['moveLegWithJointPos',['../classPyNAO.html#addc36b3747ab815f71e664f8cd417af8',1,'PyNAO']]]
 ];
